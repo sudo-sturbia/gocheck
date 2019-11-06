85cd97a9e9be0e173de4774b5b5b8d13a129d2a3
sudo-sturbia <49781778+sudo-sturbia@users.noreply.github.com>
2019-11-06 22:19:15 +0200

    Added user input through command line arguments.

--- cmd/gocheck/main.go
+++ cmd/gocheck/main.go
@@ -4,36 +4,56 @@ import (
     "fmt"
     "os"
     "bufio"
+    "flag"
+    "errors"
+    "log"
 )
 
 const SIZE_OF_ALPHABET = 26
 const FIRST_ASCII_CHAR = 97
 
 func main() {
-    loadDictionary()
+    // Process command line arguments
+    filePath := flag.String("f", "", "Path to the file that should be processed.")
+    dictionaryPath := flag.String("d", "", "Path to dictionary used in processing.")
+
+    filePathError := errors.New("option -f empty, no file specified.")
+    dictionaryPathError := errors.New("option -d empty, no dictionary file specified.")
+
+    flag.Parse()
+
+    if len(*filePath) == 0 {
+        log.Fatal(filePathError)
+    } else if len(*dictionaryPath) == 0 {
+        log.Fatal(dictionaryPathError)
+    }
+
+    loadDictionary(*dictionaryPath)
 }
+
+/*
+ * Loading a dictionary
+ */
 
 // A trie node
 type Node struct {
-    childNodes [SIZE_OF_ALPHABET]*Node    // An array of child nodes
+    children [SIZE_OF_ALPHABET]*Node      // An array of nodes
     isWord bool                           // True if node references a word ending, false otherwise
 }
 
 // Load a dictionay of words into a Trie
-func loadDictionary() *Node {
-    root := new(Node)
-
-    // Open words' file
-    file, err := os.Open("words.txt")
+func loadDictionary(path string) *Node {
+    // Open dictionary file
+    file, err := os.Open(path)
 
     if err != nil {
-        fmt.Println(err)
-        panic(err)
+        log.Fatal(err)
     }
-
     defer file.Close()
 
     // Load words from file to a trie
+    root := new(Node)
+
     fileScanner := bufio.NewScanner(file)
     for fileScanner.Scan() {
         word := fileScanner.Text()
@@ -41,7 +61,7 @@ func loadDictionary() *Node {
         root = loadWord(root, word, 0)
     }
 
-    fmt.Println(root.childNodes)
+    fmt.Println(root.children)
 
     return root
 }
@@ -55,10 +75,10 @@ func loadWord(root *Node, word string, charNumber int) *Node {
     }
 
     // If Node is not initialized
-    if root.childNodes[word[charNumber] - FIRST_ASCII_CHAR] == nil {
-        root.childNodes[word[charNumber] - FIRST_ASCII_CHAR] = new(Node)
+    if root.children[word[charNumber] - FIRST_ASCII_CHAR] == nil {
+        root.children[word[charNumber] - FIRST_ASCII_CHAR] = new(Node)
     }
 
-    root.childNodes[word[charNumber] - FIRST_ASCII_CHAR] = loadWord(root.childNodes[word[charNumber] - FIRST_ASCII_CHAR], word, charNumber + 1)
+    root.children[word[charNumber] - FIRST_ASCII_CHAR] = loadWord(root.children[word[charNumber] - FIRST_ASCII_CHAR], word, charNumber + 1)
     return root
 }
