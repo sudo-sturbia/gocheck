b369d5b65b33cdeea4f99894223fabc056a7b676
sudo-sturbia <49781778+sudo-sturbia@users.noreply.github.com>
2019-11-14 23:35:11 +0200

    Created a detailed --help flag.

--- cmd/gocheck/main.go
+++ cmd/gocheck/main.go
@@ -20,10 +20,34 @@ var wg sync.WaitGroup
 
 func main() {
     // Process command line flags
-    filePathFlag := flag.String("f", "", "Path to the file that should be processed.")
-    dictionaryPathFlag := flag.String("d", "", "Path to dictionary used in processing.")
-    uppercaseFlag := flag.Bool("u", false, "Ignore uppercase letters. By default a word that contains an uppercase letter any where but the start is considered wrong, " +
-                                           "when this flag is used uppercase and lowercase letters are treated similarly.")
+    filePathFlag := flag.String("f", "", "path to the file that should be processed.")
+    dictionaryPathFlag := flag.String("d", "", "Path to dictionary used validation.")
+    uppercaseFlag := flag.Bool("u", false, "ignore uppercase letters. By default a word that contains an uppercase letter any where but the start is considered wrong, " +
+                                           "when this flag is used, this feature is disabled.")
+
+    // Create --help flag
+    flag.Usage = func () {
+        fmt.Println(
+            "gocheck is a simple, fast spell-checker.\n" +
+            "\n" +
+            "usage:\n" +
+            "\n" +
+            "       gocheck [-h] [-f PATH] [-d PATH] [-u]\n" +
+            "\n" +
+            "required arguments:\n" +
+            "\n" +
+            "       -f PATH     Path to the file that should be processed.\n" +
+            "       -d PATH     Path to dictionary used for validation.\n" +
+            "\n" +
+            "optional arguments:\n" +
+            "\n" +
+            "       -h --help   Print this help message.\n" +
+            "       -u          Ignore uppercase letters.\n" +
+            "                   By default a word that contains an uppercase letter any where but the start is considered wrong, when this flag is used, this feature is disabled.\n" +
+            "\n" +
+            "For the source code check the github page [github.com/sudo-sturbia/gocheck]",
+        )
+    }
 
     flag.Parse()
 
